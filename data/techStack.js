01390fe8b26429eb2fc9770e0b636456062b7b8b
Mohd Farhanullah <117939387+farhan052@users.noreply.github.com>
2026-02-09 10:20:24 +0530

    Create techStack.js

---
+++ data/techStack.js
@@ -0,0 +1,7 @@
+export const techStack = [
+  { name: "Laravel", color: "from-red-400 to-red-600" },
+  { name: "Nuxt.js", color: "from-green-400 to-green-600" },
+  { name: "Next.js", color: "from-gray-200 to-gray-400" },
+  { name: "React", color: "from-cyan-400 to-cyan-600" },
+  { name: "TypeScript", color: "from-blue-400 to-blue-600" },
+];
