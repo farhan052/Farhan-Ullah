075807a4890fd5d26b8127712695e6f3dd1df53a
Mohd Farhanullah <117939387+farhan052@users.noreply.github.com>
2026-02-09 10:19:59 +0530

    Create TechSwiper.js

---
+++ components/TechSwiper.js
@@ -0,0 +1,40 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+
+import { techStack } from "@/data/techStack";
+
+const TechSwiper = () => {
+  return (
+    <div className="w-64 hidden lg:block">
+      <Swiper
+        direction="vertical"
+        modules={[Autoplay]}
+        slidesPerView={3}
+        spaceBetween={24}
+        loop
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        className="h-72"
+      >
+        {techStack.map((tech) => (
+          <SwiperSlide key={tech.name}>
+            <div
+              className={`h-20 flex items-center justify-center rounded-xl
+              bg-gradient-to-r ${tech.color}
+              text-lg font-semibold text-black shadow-lg`}
+            >
+              {tech.name}
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default TechSwiper;
